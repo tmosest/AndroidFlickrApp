a737f5e97e2af7e4fda6187dc05aea041bf320f9
AF17783 <tyler.moses@anthem.com>
2018-12-20 18:51:43 -0500

    random way to log, thing to note is how when this is compiled it will use the string MainActivty in all logs

--- app/src/main/java/com/tmosest/androidflckr/MainActivity.kt
+++ app/src/main/java/com/tmosest/androidflckr/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tmosest.androidflckr
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
@@ -11,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(tag, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
@@ -22,12 +24,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(tag, "onCreateOptionsMenu: called")
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d(tag, "onOptionsItemSelected: called")
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -35,5 +39,9 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object {
+        private const val tag =  "MainActivity"
     }
 }
