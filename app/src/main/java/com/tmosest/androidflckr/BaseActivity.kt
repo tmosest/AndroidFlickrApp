6a9114a3675f344f1856215543cb40bb011a3c58
AF17783 <tyler.moses@anthem.com>
2018-12-20 23:58:18 -0500

    fixed back arrow on details page

--- app/src/main/java/com/tmosest/androidflckr/BaseActivity.kt
+++ app/src/main/java/com/tmosest/androidflckr/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
-import android.view.View
 
 private const val TAG = "BaseActivity"
 internal const val FLICKER_QUERY = "FLICKER_QUERY"
@@ -16,8 +15,9 @@ open class BaseActivity : AppCompatActivity() {
     internal fun activateToolBar(enabledHome: Boolean) {
         Log.d(TAG, ".activateToolBar")
 
-        var toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        Log.d(TAG, ".activateToolBar was toolbar null? ${toolbar == null}")
         setSupportActionBar(toolbar)
-        supportActionBar?.setHomeButtonEnabled(enabledHome)
+        supportActionBar?.setDisplayHomeAsUpEnabled(enabledHome)
     }
 }
