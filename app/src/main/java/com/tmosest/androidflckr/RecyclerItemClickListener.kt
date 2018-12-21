7824900a033ccd0287fbca3ed6b5788711a13f93
AF17783 <tyler.moses@anthem.com>
2018-12-20 23:23:37 -0500

    got click callbacks working

--- app/src/main/java/com/tmosest/androidflckr/RecyclerItemClickListener.kt
+++ app/src/main/java/com/tmosest/androidflckr/RecyclerItemClickListener.kt
@@ -1,8 +1,10 @@
 package com.tmosest.androidflckr
 
 import android.content.Context
+import android.support.v4.view.GestureDetectorCompat
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 
@@ -16,8 +18,28 @@ class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, pr
         fun onItemLongClick(view: View, position: Int)
     }
 
+    // add gesture detector fr backwards compatibility
+    private val gestureDetector = GestureDetectorCompat(context, object: GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, "onSingleTapUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, "onSingleTapUp: calling listener.onItemClick")
+            listener.onItemClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, "onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, "onLongPress: calling listener.onItemLongClick")
+            listener.onItemLongClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, "onInterceptTouchEvent: view $rv with event $e")
-        return super.onInterceptTouchEvent(rv, e)
+        val result = gestureDetector.onTouchEvent(e)
+        Log.d(TAG, "onInterceptTouchEvent: returning $result")
+        return result
     }
 }
